3fbff307dfef84f2d7d1d4f296d67764bb762094
Guido Asbun <guido@asbun.io>
2022-12-03 13:42:44 -0800

    Layout

--- src/main/java/com/cs272/spellingbee/SpellingBeeController.java
+++ src/main/java/com/cs272/spellingbee/SpellingBeeController.java
@@ -1,16 +1,45 @@
 package com.cs272.spellingbee;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class SpellingBeeController {
+    // Instance FXML variables
     @FXML
-    private Label welcomeText;
+    private Label currentDateDisplay;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ListView<String> correctWordListView;
+
+    // Initialize function -
+    // Initial state of app before it starts
+    @FXML
+    public void initialize(){
+        currentDateDisplay.setText(getCurrentDate());
+    }
+
+    // Action handler Methods
+    @FXML
+    public void handleCloseProgramButton() {
+       // Add any additional task to do before closing app
+        Platform.exit();
+    }
+
+    @FXML
+    public void handleResetButton() {
+        System.out.println("Reset Button Pressed");
+    }
+
+    // Private Helper Functions
+    private String getCurrentDate() {
+        DateTimeFormatter formattedTime = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+        LocalDateTime currentDate = LocalDateTime.now();
+        return formattedTime.format(currentDate);
     }
 
 
